ce0e774287c5f403599e54a43de41ce327d880de
nithin6196 <nidhinnkundara@gmail.com>
2024-04-18 13:55:59 +0530

    ui and state management completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
           create: (context) => HomeScreenController(),
         )
       ],
-      child: MaterialApp(
+      child: const MaterialApp(
         home: HomeScreen(),
       ),
     );
--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -20,16 +20,15 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> imagesList = [
-      "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-      "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-      "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-      "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
-      "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
-    ];
     final provider = context.watch<HomeScreenController>();
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.shopping_cart,
+          color: Colors.black,
+        ),
+      ),
       appBar: AppBar(
         centerTitle: true,
         title: const Text(
@@ -37,49 +36,71 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(fontSize: 26, fontWeight: FontWeight.w500),
         ),
       ),
-      body: GridView.builder(
-        itemCount: provider.productsList.length,
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            childAspectRatio: 4 / 3,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 20,
-            maxCrossAxisExtent: 500),
-        itemBuilder: (context, index) => Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      provider.productsList[index].image.toString()),
-                  fit: BoxFit.cover)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                provider.productsList[index].id.toString(),
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black),
+      body: provider.isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.builder(
+              itemCount: provider.productsList.length,
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  childAspectRatio: 4 / 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 20,
+                  maxCrossAxisExtent: 500),
+              itemBuilder: (context, index) => Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            provider.productsList[index].image.toString()),
+                        fit: BoxFit.cover)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      provider.productsList[index].id.toString(),
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      provider.productsList[index].title.toString(),
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      "\$ ${provider.productsList[index].price.toString()}",
+                      style: const TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.star_border,
+                          size: 30,
+                          color: Colors.yellowAccent,
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          provider.productsList[index].rating?.rate
+                                  ?.toString() ??
+                              "",
+                          style: const TextStyle(
+                              fontSize: 22, color: Colors.greenAccent),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-              Text(
-                provider.productsList[index].title.toString(),
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black),
-              ),
-              Text(
-                provider.productsList[index].price.toString(),
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black),
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
